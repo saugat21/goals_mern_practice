20e6d9ee2d9da878f45d4aa3e953df5544747ee8
saugat21 <saugatbaral122@gmail.com>
2023-08-21 16:31:33 +0545

    updated async handler to handle error

--- Backend/controller/goalController.js
+++ Backend/controller/goalController.js
@@ -1,30 +1,33 @@
 
+const asyncHandler = require('express-async-handler')
+
+
 //get request /api/goals/  with private access
-const getGoals = (req, res) => {
+const getGoals = asyncHandler(async (req, res) => {
     res.status(200).json({ message: 'Get goals' })
-}
+})
 
 //post request /api/goals/  with private access
 
-const setGoal = (req, res) => {
+const setGoal = asyncHandler(async (req, res) => {
     if (!req.body.text) {
         res.status(400)
         throw new Error('please add a text field')
     }
     res.status(200).json({ message: 'Set goal' })
-}
+})
 
 //put request /api/goals/:id  with private access
 
-const updateGoal = (req, res) => {
+const updateGoal = asyncHandler(async (req, res) => {
     res.status(200).json({ message: `Update goals ${req.params.id}` })
-}
+})
 
 //delete request /api/goals/:id  with private access
 
-const deleteGoal = (req, res) => {
+const deleteGoal = asyncHandler(async (req, res) => {
     res.status(200).json({ message: `Update goals ${req.params.id}` })
-}
+})
 
 
 
