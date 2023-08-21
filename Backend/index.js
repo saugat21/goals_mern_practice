c425b8d1212c6414a1a3196d39e6692a9276c67a
saugat21 <saugatbaral122@gmail.com>
2023-08-21 16:50:27 +0545

    connected with database

--- Backend/index.js
+++ Backend/index.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const colors = require('colors');
 const dotenv = require('dotenv').config();
+const connectDB = require('./config/db')
 const { errorHandler } = require('./middleware/errorMiddleware')
 const port = process.env.PORT || 5000;
 const goalRoutes = require('./routes/goalRoutes')
+
+connectDB();
 
 const app = express();
 
